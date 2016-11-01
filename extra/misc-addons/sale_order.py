c0c2b8187acde3c7881f1f4e5acccd16014f4970
Gustavo Isidoro <tartuzet@gmail.com>
2016-11-01 01:56:43 +0000

    Agregue el nuevo reporte en el boton imprimir de Cotizacion

--- extra/misc-addons/sale_order.py
+++ extra/misc-addons/sale_order.py
@@ -2,6 +2,9 @@
 from openerp.osv import fields, osv
 from openerp import tools
 from datetime import datetime, timedelta
+import logging
+logging.basicConfig(level=logging.INFO)
+from openerp import netsvc
 
 class sale_order(osv.osv):
     _inherit = 'sale.order'
@@ -9,4 +12,17 @@ class sale_order(osv.osv):
        	'project': fields.char('Proyecto',size=300),
         'proposal': fields.char('Propuesta',size=300),
         }
+
+
+    def print_quotation(self, cr, uid, ids, context=None):
+        wf_service = netsvc.LocalService("workflow")
+        wf_service.trg_validate(uid, 'sale.order', ids[0], 'quotation_sent', cr)
+
+        datas = {
+                     'model': 'sale.order',
+                     'ids': ids,
+                     'form': self.read(cr, uid, ids[0], context=context),
+        }
+        return {'type': 'ir.actions.report.xml', 'report_name': 'sale.order.report', 'datas': datas, 'nodestroy': True}
+
 sale_order()
